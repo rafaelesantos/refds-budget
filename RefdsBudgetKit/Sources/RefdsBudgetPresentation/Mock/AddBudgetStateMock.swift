90fcfd529e3844adacdcb1bc284e0a8211fd9b9e
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-04-06 23:40:30 -0400

    Create redux mock states

---
+++ RefdsBudgetKit/Sources/RefdsBudgetPresentation/Mock/AddBudgetStateMock.swift
@@ -0,0 +1,13 @@
+import Foundation
+import RefdsBudgetData
+
+public struct AddBudgetStateMock: AddBudgetStateProtocol {
+    public var id: UUID = .init()
+    public var amount: Double = .random(in: 250 ... 750)
+    public var description: String? = .someParagraph()
+    public var month: Date = .current
+    public var categoryId: UUID = .init()
+    public var error: RefdsBudgetError? = Bool.random() ? nil : .existingBudget
+    
+    public init() {}
+}
---
+++ RefdsBudgetKit/Sources/RefdsBudgetPresentation/Mock/AddCategoryStateMock.swift
@@ -0,0 +1,14 @@
+import Foundation
+import SwiftUI
+import RefdsBudgetData
+
+public struct AddCategoryStateMock: AddCategoryStateProtocol {
+    public var id: UUID = .init()
+    public var name: String = .someWord()
+    public var color: Color = .random
+    public var icon: String = "dollarsign"
+    public var budgets: [AddBudgetStateProtocol] = (0 ... 4).map { _ in AddBudgetStateMock() }
+    public var error: RefdsBudgetError? = Bool.random() ? nil : .existingCategory
+    
+    public init() {}
+}
